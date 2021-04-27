466e68bb35fe245bbf84aafe4bf1791ff768a527
marcus <marcus.longoria12@gmail.com>
2021-04-27 01:27:52 -0500

    commit tree 05f247be46e278e31c030251fe893bff3f281bc3

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updatedDOM()
+});
+
+function updatedDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
